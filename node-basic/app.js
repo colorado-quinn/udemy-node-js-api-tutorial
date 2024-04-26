383791bb501f63cce7386efb9595fd547557b02e
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-26 00:00:42 -0600

    refactor to helpers

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,10 +1,8 @@
 // to execute in terminal:  node app.js
+const helpers = require('./helpers');
 
-console.log('hello from node js');
+//console.log('hello from node js');
+//console.log('process: ', process);
 
-function sum(a, b) {
-    return a + b;
-}
-
-const total = sum(10, 200);
+const total = helpers.sum(10, 200);
 console.log('total: ', total);
---
+++ node-basic/helpers.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = {
+    sum,
+};
+
+//console.log('process: ', process);
+// prints:  exports: { sum: [Function: sum] },
