52db32dc0d44f7856cb444238a9908b746560a62
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-25 23:44:53 -0600

    add function

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,3 +1,10 @@
+// to execute in terminal:  node app.js
+
 console.log('hello from node js');
 
-// to execute in terminal:  node app.js
+function sum(a, b) {
+    return a + b;
+}
+
+const total = sum(10, 200);
+console.log('total: ', total);
