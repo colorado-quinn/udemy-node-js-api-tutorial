6d020e49961e0f9602371a81f75387ffe4556f24
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-26 22:28:07 -0600

    19 add secrets notes

--- node-basic/eventloop.js
+++ node-basic/eventloop.js
@@ -18,3 +18,10 @@ const handleError = (err) => console.log(err);
 const handleData = (data) => console.log(data.toString());
 
 console.log('node.js is asynchronous, single threaded, non blocking event loop');
+
+/** Secrets of understanding node js
+ *  - JavaScript environment browser vs server
+ *  - Understanding functions and callback functions
+ *  - event loop
+ *  - asynchronous (non-blocking) vs synchronous (blocking)
+ */
