22453547ee10e58fa8e00a9a2f067d20d8aceae9
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-26 22:16:16 -0600

    17 add synchronous example

--- node-basic/eventloop.js
+++ node-basic/eventloop.js
@@ -2,14 +2,18 @@ const fs = require('fs');
 
 const fileName = 'target.txt';
 //fs.watch(fileName, () => console.log('file changed'));
+
+// sync
+// const data = fs.readFileSync(fileName);
+// console.log(data.toString());
+
+// async
 fs.readFile(fileName, (err, data) => {
     if (err) {
         console.log(error);
     }
 
-    console.log(data);
+    console.log(data.toString());
 });
 
-console.log(
-    'node.js is asynchronous, single threaded, non blocking event loop'
-);
+console.log('node.js is asynchronous, single threaded, non blocking event loop');
