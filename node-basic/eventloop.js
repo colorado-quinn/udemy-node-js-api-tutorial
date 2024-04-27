6b1d43ad646ed51b7619a1ac28d34192ad6d154d
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-26 21:48:23 -0600

    16 async event loop

--- node-basic/eventloop.js
+++ node-basic/eventloop.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 
 const fileName = 'target.txt';
-fs.watch(fileName, () => console.log('file changed'));
+//fs.watch(fileName, () => console.log('file changed'));
+fs.readFile(fileName, (err, data) => {
+    if (err) {
+        console.log(error);
+    }
+
+    console.log(data);
+});
+
+console.log(
+    'node.js is asynchronous, single threaded, non blocking event loop'
+);
