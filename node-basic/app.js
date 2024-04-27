157a31402f2f38e3e6b43f0f2ef2cc3c99326732
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-26 20:51:52 -0600

    13 express server

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,15 +1,21 @@
 // to execute in terminal:  node app.js
 const { sum } = require('./helpers');
-const { createServer } = require('http');
+//const { createServer } = require('http'); // core module
+const express = require('express');
 
-//console.log('hello from node js');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello from express :)');
+});
+app.listen(3000);
+
 //console.log('process: ', process);
 
-const server = createServer((req, res) => {
-    res.end('hello world from node js server :D');
-});
+// const server = createServer((req, res) => {
+//     res.end('hello world from node js server :D');
+// });
+// server.listen(3000);
 
-server.listen(3000);
-
-const total = sum(10, 200);
-console.log('total: ', total);
+// const total = sum(10, 200);
+// console.log('total: ', total);
