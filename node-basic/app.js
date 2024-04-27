9a10874ab1ce88d7cd33b543bea69700af576595
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-26 20:27:45 -0600

    import http module and create server

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,8 +1,15 @@
 // to execute in terminal:  node app.js
 const { sum } = require('./helpers');
+const http = require('http');
 
 //console.log('hello from node js');
 //console.log('process: ', process);
+
+const server = http.createServer((req, res) => {
+    res.end('hello world from node js :D');
+});
+
+server.listen(3000);
 
 const total = sum(10, 200);
 console.log('total: ', total);
