219440a5c41ec0f3095515db0d8fd31c4abdd3b9
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-01 13:44:52 -0600

    33 add validation

--- node-api/helpers/useRouting.js
+++ node-api/helpers/useRouting.js
@@ -1,11 +1,13 @@
 const { useApp } = require('./useApp');
 const express = require('express');
 const postController = require('../controllers/post');
+const useValidation = require('./useValidation');
 
 exports.useRouting = () => {
+    const { postValidation } = useValidation();
     const router = express.Router();
     router.get('/', postController.getPosts);
-    router.post('/post', postController.createPost);
+    router.post('/post', postValidation(), postController.createPost);
 
     const app = useApp();
     app.use('/', router);
---
+++ node-api/helpers/useValidation.js
@@ -0,0 +1,36 @@
+const { body, validationResult } = require('express-validator');
+
+const postValidation = () => {
+    return async (req, res, next) => {
+        console.log('post validation!');
+
+        // title
+        await body('title').trim().notEmpty().withMessage('A title is required.').run(req);
+        await body('title')
+            .trim()
+            .isLength({ min: 4, max: 150 })
+            .withMessage('The title must be between 4 and 150 characters.')
+            .run(req);
+
+        // body
+        await body('body').trim().notEmpty().withMessage('A body is required.').run(req);
+        await body('body')
+            .trim()
+            .isLength({ min: 4, max: 2000 })
+            .withMessage('The body must be between 4 and 2000 characters.')
+            .run(req);
+
+        const errors = validationResult(req);
+        //console.log('val result: ', errors.array());
+        if (errors.isEmpty()) {
+            return next();
+        }
+
+        const firstError = errors.array().map((err) => err.msg)[0];
+        return res.status(400).json({ error: firstError });
+    };
+};
+
+const useValidation = () => ({ postValidation });
+
+module.exports = useValidation;
