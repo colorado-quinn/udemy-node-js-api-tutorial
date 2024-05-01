ee7db484b74d59cfe2991606ab067950c989a858
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-01 14:05:44 -0600

    34 read posts

--- node-api/controllers/post.js
+++ node-api/controllers/post.js
@@ -1,20 +1,18 @@
 const Post = require('../models/post');
 
 exports.getPosts = (req, res) => {
-    res.json({
-        posts: [{ title: 'First post!' }, { title: 'Second post' }],
-    });
+    Post.find()
+        //.select(['_id', 'title', 'body'])
+        .select('-__v') // remove __v property
+        .then((posts) => res.json({ posts }))
+        .catch((error) => res.status(400).json({ error }));
 };
 
 exports.createPost = async (req, res) => {
     console.log('Creating post: ', req.body);
 
-    try {
-        const post = new Post(req.body);
-        const result = await post.save();
-
-        return res.status(200).json({ post: result });
-    } catch (err) {
-        return res.status(400).json({ error: err });
-    }
+    const post = new Post(req.body);
+    post.save()
+        .then((result) => res.json({ post: result }))
+        .catch((error) => res.status(400).json({ error }));
 };
