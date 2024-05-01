2877c084a9817e55cc89b048ec0f29e83302274c
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-04-30 22:24:47 -0600

    26 connect to mongodb

--- node-api/app.js
+++ node-api/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const { useMiddleware, useRouting } = require('./middleware');
 
-const port = 8080;
+dotenv.config();
+
+// db
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('DB connected'));
+mongoose.connection.on('error', (err) => console.log(`DB connection error: ${err.message}`));
+
+const port = process.env.PORT || 8080;
 const app = express();
 
 useMiddleware(app);
